70d61dcf366763f48374f3739e95d55454d45e8e
Ruslan Tarutin <tarutin.ruslan99@gmail.com>
2024-02-09 20:24:26 +0300

    rm debugging code

--- internal/app/dao/hash_map_storage.go
+++ internal/app/dao/hash_map_storage.go
@@ -1,35 +1,27 @@
 package dao
 
 
-import "fmt"
-
-
 type HashMapDAO struct {
 	storage map[string]string
 }
 
 func (dao *HashMapDAO) GetURLByShortLink(shortLink string) (string, error) {
-	fmt.Println(dao.storage)
 
 	if dao.storage == nil {
 		dao.storage = make(map[string]string)
 	}
 
 	if value, ok := dao.storage[shortLink]; ok {
-		fmt.Println(value, ok)
 		return value, nil
 	} else {
-		fmt.Println(value, ok)
 		return value, ErrURLMappingNotFound
 	}
 }
 
 func (dao *HashMapDAO) AddShortURL(shortLink string, fullLink string) error {
-	fmt.Println("Before", dao.storage)
 	if dao.storage == nil {
 		dao.storage = make(map[string]string)
 	}
 	dao.storage[shortLink] = fullLink
-	fmt.Println("After", dao.storage)
 	return nil
 }
