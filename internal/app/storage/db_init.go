0aae10934988838dcdc14f8f2fb9b36234003c3e
Ruslan Tarutin <tarutin.ruslan99@gmail.com>
2024-03-24 13:20:09 +0300

    increased column size

--- internal/app/storage/db_init.go
+++ internal/app/storage/db_init.go
@@ -12,7 +12,7 @@ func InitPostgres(db *sql.DB) {
 		id serial CONSTRAINT urls_pl PRIMARY KEY,
 		url varchar(512),
 		alias varchar(512),
-		uuid varchar(32),
+		uuid varchar(64),
 		CONSTRAINT url_uniq UNIQUE (url),
 		CONSTRAINT alias_uniq UNIQUE (alias)
 	);
